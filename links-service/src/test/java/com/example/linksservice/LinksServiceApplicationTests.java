0032823bb29ada24a23417a8122a70dc6bb66d5e
voidnowhere <alivnw@outlook.com>
2023-12-18 00:14:23 +0100

    init

---
+++ links-service/src/test/java/com/example/linksservice/LinksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linksservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
