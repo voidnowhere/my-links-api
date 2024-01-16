8fc2571a03de2671f27bd793cb68142dc23b3c1d
voidnowhere <alivnw@outlook.com>
2024-01-16 15:34:40 +0100

    add docker run cmd in readme

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Architecture
 
 ![Architecture](https://github.com/voidnowhere/my-links-api/assets/79842485/c2a7278c-12a1-439f-a98b-6ab225463b27)
+
+## Run on docker
+```bash
+docker-compose up -d
+```
--- gateway-service/src/main/java/com/example/gatewayservice/config/GatewayConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/GatewayConfig.java
@@ -3,12 +3,10 @@ package com.example.gatewayservice.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableHystrix
 @RequiredArgsConstructor
 public class GatewayConfig {
     private final AuthFilter filter;
